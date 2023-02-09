b7ec469d3311729cd581f5bf1f0ed4f4e8fe7058
angelehl <secahl02@gmail.com>
2023-02-09 13:24:43 -0600

    Se añade filtering y se inicia con jpa

--- src/main/java/com/angelehl/rest/webservices/restfulwebservices/jpa/UserResource.java
+++ src/main/java/com/angelehl/rest/webservices/restfulwebservices/jpa/UserResource.java
@@ -1,10 +1,13 @@
-package com.angelehl.rest.webservices.restfulwebservices.user;
+package com.angelehl.rest.webservices.restfulwebservices.jpa;
+
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -16,24 +19,36 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.angelehl.rest.webservices.restfulwebservices.user.User;
+import com.angelehl.rest.webservices.restfulwebservices.user.UserDaoService;
+import com.angelehl.rest.webservices.restfulwebservices.user.UserNotFoundException;
+
 import jakarta.validation.Valid;
 
 @RestController
-public class UserResoruce {
+public class UserResource {
 	
-	@Autowired
-	private UserDaoService service; 	
 	
-	public UserResoruce(UserDaoService service){
+	private UserDaoService service; 
+	
+	private UserRepository repository;
+	
+	public UserResource(UserDaoService service, UserRepository repository){
 		
 		this.service = service;
+		this.repository = repository;
 	}
 	
 	//GETUsers
-	@GetMapping("/users")
+	/*@GetMapping("/users")
 	public List<User> retrieveAllUsers(){
 		
 		return service.findAll();
+	}*/
+	@GetMapping("/jpa/users")
+	public List<User> retrieveAllUsers(){
+		
+		return repository.findAll();
 	}
 	
 	//GET one user in specific
@@ -49,7 +64,7 @@ public class UserResoruce {
 		
 	}*/
 	//-----------------------HATEOAS-------------------------
-	@GetMapping("/users/{id}")
+	/*@GetMapping("/users/{id}")
 	public EntityModel<User> retrieveUser(@PathVariable int id){
 		
 		User user=service.findUser(id);
@@ -64,9 +79,29 @@ public class UserResoruce {
 		
 		return entityModel;
 		
+	}*/
+	
+	@GetMapping("/jpa/users/{id}")
+	public EntityModel<User> retrieveUser(@PathVariable int id){
+		
+		Optional<User> user=repository.findById(id);
+		
+		if(user.isEmpty())
+			throw new UserNotFoundException("id: "+id);
+		
+		EntityModel<User> entityModel = EntityModel.of(user.get());
+		
+		WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+		entityModel.add(link.withRel("all-users"));
+		
+		return entityModel;
+		
 	}
+	
+	
+	
 	//----------------------------------------------------------
-	@PostMapping("/users")
+	/*@PostMapping("/users")
 	public ResponseEntity<User> createUser(@Valid @RequestBody User user){
 		
 		User savedUser = service.createUser(user);
@@ -76,14 +111,32 @@ public class UserResoruce {
 						.buildAndExpand(savedUser.getId()).toUri();
 		
 		return  ResponseEntity.created(location).build();
-	}
+	}*/
 	
-	@DeleteMapping("/users/{id}")
+	/*@DeleteMapping("/users/{id}")
 	public void deleteUser(@PathVariable int id){
 		
 		service.deleteById(id);
 		
+	}}**/
+	
+	@DeleteMapping("/jpa/users/{id}")
+	public void deleteUser(@PathVariable int id){
+		
+		repository.deleteById(id);
+		
 	}
 	
-
+	@PostMapping("/jpa/users")
+	public ResponseEntity<User> createUser(@Valid @RequestBody User user){
+		
+		User savedUser = repository.save(user);
+		
+		//Se añade la ubicación (URI) en el header del recurso creado 
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+						.buildAndExpand(savedUser.getId()).toUri();
+		
+		return  ResponseEntity.created(location).build();
+	}
 }
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user_details(id,birth_date,name) values(10001,current_date(),'angel');
+insert into user_details(id,birth_date,name) values(10002,current_date(),'david');
+insert into user_details(id,birth_date,name) values(10003,current_date(),'rosa');
